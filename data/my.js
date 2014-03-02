a42dbe81b74e28722f01d7da6bb62342771b79cc
Ganesh <ganesh@ganesh-desktop.(none)>
2014-03-03 02:50:22 +0530

    add on completed

--- data/my.js
+++ data/my.js
@@ -2,29 +2,47 @@
  // document.body.innerHTML = "<h1>" + message + "</h1>";
 //});
 $('document').ready(function(){
+//myalert("Test", "This is a test modal dialog");
     $('body').dblclick(function(){
        var word=getSelectedWord();
-       $.ajax({
-         type : "GET",
-         contentType: 'application/jsonp',
-          data : {get_param : "Announcement"},
-         dataType:'jsonp',
-          jsonp:'onJSONPLoad',
-         crossDomain:true,
-         url:"http://corephp-ganeshpatil.rhcloud.com/api.php",
-         success: function(data) {
-             console.log("Success"+data);
-          },
-         error: function (data,string,actualError) {
-              console.log("error"+data);
-              console.log("string" + string);
-              console.log("actual error:" + actualError);
-          }
+ //      $.ajax({
+     //    type : "GET",
+   //      contentType: 'application/jsonp',
+       //   data : {get_param : "Announcement"},
+        // dataType:'jsonp',
+        ///  jsonp:'onJSONPLoad',
+         //crossDomain:true,
+        // url:"http://corephp-ganeshpatil.rhcloud.com/api.php",
+        // success: function(data) {
+        //     console.log("Success"+data);
+        //  },
+        // error: function (data,string,actualError) {
+        //      console.log("error"+data);
+        //      console.log("string" + string);
+        //      console.log("actual error:" + actualError);
+         // }
 
 
-         });
+         //});
+//           alert('clicked');
+
+      self.port.emit("double",word);
 });
 });
+self.port.on("meaning",function(meaning){
+     var word_meaning='';
+     $.each(meaning['tuc'] ,function (key,value){
+        //  if(value=='meanings') {
+                $.each(value['meanings'], function (value_key,value_meaning) {
+                             console.log(value_meaning['text']);
+                           word_meaning =value_meaning['text'];
+                   });
+          //  }
+});
+  alert(word_meaning);
+});
+
+
 
 function getSelectedWord(){
 var text = '';
