81d3a5acb5cdf0d2909d582cce924f5215138e41
ganesh.webonise <ganesh@weboniselab.com>
2014-03-06 19:28:09 +0530

    changes

--- data/my.js
+++ data/my.js
@@ -1,44 +1,28 @@
-//self.port.on("replacePage", function(message) {
- // document.body.innerHTML = "<h1>" + message + "</h1>";
-//});
 $('document').ready(function(){
 //myalert("Test", "This is a test modal dialog");
     $('body').dblclick(function(){
        var word=getSelectedWord();
- //      $.ajax({
-     //    type : "GET",
-   //      contentType: 'application/jsonp',
-       //   data : {get_param : "Announcement"},
-        // dataType:'jsonp',
-        ///  jsonp:'onJSONPLoad',
-         //crossDomain:true,
-        // url:"http://corephp-ganeshpatil.rhcloud.com/api.php",
-        // success: function(data) {
-        //     console.log("Success"+data);
-        //  },
-        // error: function (data,string,actualError) {
-        //      console.log("error"+data);
-        //      console.log("string" + string);
-        //      console.log("actual error:" + actualError);
-         // }
-
-
-         //});
-//           alert('clicked');
-
       self.port.emit("double",word);
 });
 });
 self.port.on("meaning",function(meaning){
-     var word_meaning='';
-     $.each(meaning['tuc'] ,function (key,value){
-        //  if(value=='meanings') {
+     var word_meaning='<ul>';
+    console.log(meaning);
+    console.log("Tuc: "+ meaning['tuc']);
+
+    if(typeof meaning['tuc'] !== 'undefined') {
+        $.each(meaning['tuc'] ,function (key,value){
+            if(typeof value['meanings'] !== 'undefined') {
+                console.log('value Meaning= ' + value['meanings']);
                 $.each(value['meanings'], function (value_key,value_meaning) {
-                             console.log(value_meaning['text']);
-                           word_meaning =value_meaning['text'];
-                   });
-          //  }
-});
+//                             console.log(value_meaning['text']);
+                    word_meaning +='<li>'+value_meaning['text'] +'</li>';
+                });
+            }
+
+        });
+    }
+word_meaning+='</ul>';
   alert(word_meaning);
 });
 
